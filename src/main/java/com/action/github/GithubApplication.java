7a8344f84d256865fbcf736f102360d83a24487e
Jhonatan <jhonatan@MacBook-Pro-de-Jhonatan.local>
2024-05-09 21:57:16 -0300

    first commit

---
+++ src/main/java/com/action/github/GithubApplication.java
@@ -0,0 +1,10 @@
+package com.action.github;
+
+
+public class GithubApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+	}
+
+}
---
+++ src/test/java/com/action/github/GithubApplicationTests.java
@@ -0,0 +1,17 @@
+package test.java.com.action.github;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
